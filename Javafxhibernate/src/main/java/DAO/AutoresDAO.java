766b2461e6c7f384eb3e98dc9614cb1e9b64dbe2
97ivangarcia <97ivangarcia@gmail.com>
2025-02-07 20:31:12 +0100

    AutoresDAO y SociosDAO creadas

--- Javafxhibernate/src/main/java/DAO/AutoresDAO.java
+++ Javafxhibernate/src/main/java/DAO/AutoresDAO.java
@@ -1,4 +1,42 @@
 package DAO;
 
+import entities.Autores;
+
+import java.util.List;
+
 public interface AutoresDAO {
+
+    /**
+     * Añade un nuevo autor especificando nombre y nacionalidad
+     * @param autores
+     * @return autor agregado
+     */
+    Autores create(Autores autores);
+
+    /**
+     * Modifica la información de un autor existente
+     * @param autor
+     * @return autor actualizado
+     */
+    Autores update(Autores autor);
+
+    /**
+     * Elimina un autor por su ID
+     * @param id
+     * @return true si fue eliminado, false en caso contrario
+     */
+    boolean deleteById(Integer id);
+
+    /**
+     * Busca autores por nombre
+     * @param nombre
+     * @return lista de autores que coinciden con el nombre
+     */
+    List<Autores> findByName(String nombre);
+
+    /**
+     * Lista todos los autores
+     * @return lista de autores
+     */
+    List<Autores> findAll();
 }
--- Javafxhibernate/src/main/java/DAO/SociosDAO.java
+++ Javafxhibernate/src/main/java/DAO/SociosDAO.java
@@ -1,4 +1,37 @@
 package DAO;
 
+import entities.Socios;
+
+import java.util.List;
+
 public interface SociosDAO {
+
+    Socios create(Socios socios);
+
+    /**
+     * Modifica la información de un socio existente
+     * @param socios
+     * @return socio actualizado
+     */
+    Socios update(Socios socios);
+
+    /**
+     * Elimina un socio por su ID
+     * @param id
+     * @return true si fue eliminado, false en caso contrario
+     */
+    boolean deleteById(Integer id);
+
+    /**
+     * Busca socios por nombre o número de teléfono
+     * @param criterio
+     * @return lista de socios que coinciden con el criterio
+     */
+    List<Socios> findByCriteria(String criterio);
+
+    /**
+     * Lista todos los socios
+     * @return lista de socios
+     */
+    List<Socios> findAll();
 }
