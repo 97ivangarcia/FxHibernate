36e3fc6b3e99d005bb8daaa724661878f01df635
97ivangarcia <97ivangarcia@gmail.com>
2025-02-07 20:11:06 +0100

    Clase autores añadida

--- Javafxhibernate/src/main/java/entities/Autores.java
+++ Javafxhibernate/src/main/java/entities/Autores.java
@@ -1,4 +1,54 @@
 package entities;
 
-public class Autores {
+import jakarta.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Autores")
+public class Autores implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column
+    private String nacionalidad;
+
+    // Constructor vacío
+    public Autores() {}
+
+    // Constructor con parámetros
+    public Autores(Integer id, String nombre, String nacionalidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+    }
+
+    // Getters y Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
 }
